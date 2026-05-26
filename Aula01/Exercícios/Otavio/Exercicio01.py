bf029a66ae9d156a1411cbf4338a8b582c93b6cd
Otavio <tataumiguel860@gmail.com>
2026-05-25 21:29:48 -0300

    Ediçao do exercicio 01

---
+++ Aula01/Exercícios/Otavio/Exercicio01.py
@@ -0,0 +1,13 @@
+
+# Criar a funçao
+def calcular_media(nota1, nota2):
+    media = (int(nota1) + int(nota2))
+    
+    return media
+
+
+n1 =  ("Digite a prieira nota: ")
+n1 = input("Digite a segunda nota: ")
+
+print(alcular_media(n1, n2))
+
